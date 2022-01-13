df93cf65e5d01a06040b611756cdcc9478b01264
Andrei Paleyes <apalees@gmail.com>
2022-01-13 15:35:41 +0000

    Added function of 3 objectives

--- docs/notebooks/true_pf/generate_true_pareto_fronts.py
+++ docs/notebooks/true_pf/generate_true_pareto_fronts.py
@@ -147,10 +147,44 @@ def save_hartmann_ackley_6d_input():
     print("Saved to " + HARTMANN_ACKLEY_FILENAME)
 
 
+############################
+# DTLZ 2 - 3 objectives, 4d input
+############################
+DTLZ2_FILENAME = "dtlz2_4d_input.csv"
+
+def save_dtlz2_4d_input():
+    search_space = trieste.space.Box([0, 0, 0, 0], [1, 1, 1, 1])
+
+    def g(x): 
+        return tf.pow(x[:, 2] - 0.5, 2) + tf.pow(x[:, 3] - 0.5, 2)
+
+    def f1(x):
+        value = (1.0 + g(x)) * tf.cos(x[:, 0] * math.pi * 0.5) * tf.cos(x[:, 1] * math.pi * 0.5)
+        return value[:, None]
+
+    def f2(x):
+        value = (1.0 + g(x)) * tf.cos(x[:, 0] * math.pi * 0.5) * tf.sin(x[:, 1] * math.pi * 0.5)
+        return value[:, None]
+
+    def f3(x):
+        value = (1.0 + g(x)) * tf.sin(x[:, 0] * math.pi * 0.5)
+        return value[:, None]
+
+    def f(x):
+        return tf.concat([f1(x), f2(x), f3(x)], axis=-1)
+
+    print("Generating true Pareto front for DTLZ 2")
+    problem = PymooProblem(search_space, f, n_obj=3)
+    true_pf = get_true_pf(problem)
+    save_true_pf(true_pf, DTLZ2_FILENAME)
+    print("Saved to " + DTLZ2_FILENAME)
+
+
 ############
 
 if __name__ == "__main__":
-    save_simple_1d_input()
-    save_gardner_2d_input()
-    save_hartmann_ackley_6d_input()
-    save_zdt3()
+    # save_simple_1d_input()
+    # save_gardner_2d_input()
+    # save_hartmann_ackley_6d_input()
+    # save_zdt3()
+    save_dtlz2_4d_input()
